240ffb668768fdbc01ef5f6e2256559fd721f0f2
Pankaj Pahade <pankaj.pahade@perficient.com>
2019-06-03 16:19:56 +0530

    Lab 4 Changes

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.util.List;
 
-@RestController("/time-entries")
+@RestController
+@RequestMapping("/time-entries")
 public class TimeEntryController {
 
     @Autowired
