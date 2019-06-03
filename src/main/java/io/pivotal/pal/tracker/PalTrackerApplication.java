cfe36989500adbd697aad10bac7e9185dcf508e2
Pankaj Pahade <pankaj.pahade@perficient.com>
2019-06-03 23:19:32 +0530

    cloudNativeDeveloperDatabaseAccess lab

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -15,9 +16,14 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    @Bean
+   /* @Bean
     public TimeEntryRepository  timeEntryRepository () {
-        return new InMemoryTimeEntryRepository ();
+        return new JdbcTimeEntryRepository();
+    }*/
+
+    @Bean
+    JdbcTimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     @Bean
